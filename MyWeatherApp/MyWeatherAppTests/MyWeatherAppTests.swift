2d95ce53fd92c5bf8ee687e7c0bd24259cfb9bf8
Apeksha Sahu <ApekshaSahu@users.noreply.github.com>
2018-10-12 21:21:51 -0400

    Add files via upload

---
+++ MyWeatherApp/MyWeatherAppTests/MyWeatherAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyWeatherAppTests.swift
+//  MyWeatherAppTests
+//
+//  Created by Apeksha Sahu on 10/12/18.
+//  Copyright © 2018 Apeksha Sahu. All rights reserved.
+//
+
+import XCTest
+@testable import MyWeatherApp
+
+class MyWeatherAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
